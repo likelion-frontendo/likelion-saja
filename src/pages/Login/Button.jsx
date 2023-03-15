3aea1d0ce9cc25b23009e803cd1dac30b46eaa2f
kimmoonju-102 <kimmun3549@naver.com>
2023-03-15 16:03:07 +0900

    [코드 수정] #13 Button 수정
    - 코드 공백 제거, 부모와 자식 스타일을 구분

--- src/pages/Login/Button.jsx
+++ src/pages/Login/Button.jsx
@@ -5,28 +5,27 @@ export function Button() {
     <StyledButton>
       <button type="button">로그인</button>
     </StyledButton>
-    
   );
 }
 
 const StyledButton = styled.div `
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    flex-flow: column;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-flow: column;
 
-  button {
-    height: 54px;
-    width: 340px;
-    line-height: 54px;
-    border-radius: 4px;
-    user-select: none;
-    border: none;
-    cursor: pointer;  
-    margin:0 auto;
-    font-size: 16px;
-    font-weight: 600;
-    background-color: #6C816D;
-    color: #fff;
-  }
+    button {
+      height: 54px;
+      width: 340px;
+      line-height: 54px;
+      border-radius: 4px;
+      user-select: none;
+      border: none;
+      cursor: pointer;  
+      margin:0 auto;
+      font-size: 16px;
+      font-weight: 600;
+      background-color: #6C816D;
+      color: #fff;
+    }
 `;
