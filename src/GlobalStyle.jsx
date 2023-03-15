97fbef2d95067f2e398f7207fc7442d7904e1d4e
rlawlsdn263 <rlawlsdn263@naver.com>
2023-03-15 11:56:22 +0900

    [세팅] CRA 스케폴딩 추가
    - index.html에서 불필요한 메타 태그와 주석 삭제
    - index.html에 프리텐다드 폰트 CDN 삽입
    - GlobalStyle.jsx에 글로벌 폰트 설정 및 reset.css에서 처리되는 CSS 설정 삭제

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -3,10 +3,8 @@ import reset from 'styled-reset';
 
 export const GlobalStyle = createGlobalStyle`
   ${reset}
-
   *, *::after, *::before {
-    margin: 0;
-    padding: 0;
     box-sizing: border-box;
+    font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
   }
 `
