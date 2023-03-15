91d938aa46e28f824cb43415e3fd2f4c74048c3d
kimmoonju-102 <kimmun3549@naver.com>
2023-03-15 15:42:22 +0900

    [스타일] LinkButton 생성 (마크업 &스타일)
    - 회원가입 button 성생

---
+++ src/pages/Login/LinkButton.jsx
@@ -0,0 +1,36 @@
+import styled from "styled-components/macro";
+import { Link } from 'react-router-dom';
+
+export function LinkButton() {
+  return (
+    <StyledLink>
+      <Link to="./" className="register" >회원가입</Link>
+    </StyledLink>
+    
+  );
+}
+
+const StyledLink = styled.div `
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-flow: column;
+
+  .register {
+    height: 54px;
+    width: 340px;
+    line-height: 54px;
+    border:2px solid #6C816D;
+    border-radius: 4px;
+    user-select: none;
+    cursor: pointer;  
+    margin:0 auto;
+    font-size: 16px;
+    font-weight: 600;
+    background-color: #fff;
+    color: #6C816D;
+    margin-top: 12px;
+    text-align: center;
+    text-decoration: none;
+  }
+`;
