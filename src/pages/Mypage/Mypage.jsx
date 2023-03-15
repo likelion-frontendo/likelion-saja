31b03e4b59e8bfff20f799ee1196f350d74659d2
Simyumi <gkghgkgh0@naver.com>
2023-03-15 15:21:38 +0900

    [스타일] Mypage사이드바 마크업
    SidebarTop+SidebarBottom컴포넌트추가했습니다.

---
+++ src/pages/Mypage/Mypage.jsx
@@ -0,0 +1,18 @@
+import Sidebar from "@/pages/Mypage/SidebarTop";
+import styled from "styled-components/macro";
+
+function Mypage() {
+  return (
+    <Wrap>
+      <Sidebar></Sidebar>
+    </Wrap>
+  );
+}
+
+const Wrap = styled.div`
+  width: 1056px;
+  height: 100vh;
+  margin: 60px auto 0 auto;
+  background-color: lightpink;
+`;
+export default Mypage;
---
+++ src/pages/Mypage/SidebarTop.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components/macro";
+import SidebarBottom from "@/pages/Mypage/SidebarBottom";
+
+function SidebarTop() {
+  return (
+    <MypageSide>
+      <div className="profile">
+        <img src={require("@/assets/Mypage/유네찌.png")} alt="프로필 이미지"></img>
+        <p className="profileName">유미</p>
+        <p className="profileNum">#1817221</p>
+      </div>
+      <SidebarBottom></SidebarBottom>
+    </MypageSide>
+  );
+}
+
+const MypageSide = styled.div`
+  width: 216px;
+  height: 598px;
+  /* profile */
+  & .profile {
+    height: 220px;
+    border: 1px solid #6c816d;
+    background-color: #fff;
+    border-radius: 12px 12px 0 0;
+    text-align: center;
+    padding-top: 24px;
+  }
+  & .profile img {
+    border-radius: 50%;
+    width: 100px;
+    height: 100px;
+  }
+  & .profile .profileName {
+    margin-top: 16px;
+    color: #6c816d;
+    font-size: 16px;
+    font-weight: 600;
+  }
+  & .profile .profileNum {
+    margin-top: 8px;
+    color: #868b94;
+    font-size: 14px;
+  }
+`;
+export default SidebarTop;
