d20935a2e0aeeef927511e75da10e8bebf47f65b
rlawlsdn263 <rlawlsdn263@naver.com>
2023-03-15 15:10:39 +0900

    [스타일] <Login> 페이지 컴포넌트에 <FormInput> 컴포넌트 추가 및 스타일 추가
    - <FormInput> 컴포넌트를 import 해옴
    - main, h1, input에 각각 CSS 스타일을 부여함

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,17 +1,32 @@
 import styled from 'styled-components/macro';
+import { FormInput } from '@/pages/Login/FormInput';
 
 export function Login() {
   return(
       <StyledMain>
         <h1>로그인</h1>
-        <form></form>
+        <form>
+          <FormInput name="아이디" type="text" placeholder="아이디를 입력해주세요"></FormInput>
+          <FormInput name="비밀번호" type="password" placeholder="비밀번호를 입력해주세요"></FormInput>
+        </form>
       </StyledMain>
   )
 }
 
 const StyledMain = styled.main `
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  
   h1 {
     font-size: 22px;
     font-weight: 600;
+    margin-bottom: 44px;
+  }
+
+  input {
+    margin-bottom: 12px;
   }
 `
