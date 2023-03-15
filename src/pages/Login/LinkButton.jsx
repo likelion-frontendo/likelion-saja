4b54b789e8bcce58e146bfcee8a9497b916589d5
kimmoonju-102 <kimmun3549@naver.com>
2023-03-15 16:05:14 +0900

    [코드 수정] #14 LinkButton 수정
    - 코드 공백 제거, 부모와 자식 스타일을 구분

--- src/pages/Login/LinkButton.jsx
+++ src/pages/Login/LinkButton.jsx
@@ -6,31 +6,30 @@ export function LinkButton() {
     <StyledLink>
       <Link to="./" className="register" >회원가입</Link>
     </StyledLink>
-    
   );
 }
 
 const StyledLink = styled.div `
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    flex-flow: column;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-flow: column;
 
-  .register {
-    height: 54px;
-    width: 340px;
-    line-height: 54px;
-    border:2px solid #6C816D;
-    border-radius: 4px;
-    user-select: none;
-    cursor: pointer;  
-    margin:0 auto;
-    font-size: 16px;
-    font-weight: 600;
-    background-color: #fff;
-    color: #6C816D;
-    margin-top: 12px;
-    text-align: center;
-    text-decoration: none;
-  }
+    .register {
+      height: 54px;
+      width: 340px;
+      line-height: 54px;
+      border:2px solid #6C816D;
+      border-radius: 4px;
+      user-select: none;
+      cursor: pointer;  
+      margin:0 auto;
+      font-size: 16px;
+      font-weight: 600;
+      background-color: #fff;
+      color: #6C816D;
+      margin-top: 12px;
+      text-align: center;
+      text-decoration: none;
+    }
 `;
