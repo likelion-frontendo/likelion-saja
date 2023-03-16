d16b347e9e7a9f3cd5339b352114341993952c4a
rodwnl <88178123+rodwnl@users.noreply.github.com>
2023-03-16 15:37:35 +0900

    [기타] product 폴더 생성 후 jsx 파일 이동

--- src/pages/Home/Store.jsx
+++ src/pages/Home/Store.jsx
@@ -12,14 +12,18 @@ export function Store() {
         </Link>
         <div className="storeContainer">
           <div className="storeInfo">
-            <span className="title">임은하푸드 인천창고</span>
-            <span className="location">천곡동</span>
+            <span className="title" tabIndex={0}>
+              임은하푸드 인천창고
+            </span>
+            <span className="location" tabIndex={0}>
+              천곡동
+            </span>
           </div>
-          <span className="description">
+          <span className="description" tabIndex={0}>
             임은하푸드는 전국에 수산물 창고를 둔 곳입니다. 매일 아침 창고 소식을 전해요. 단골맺기를 하시면 소식 알람이 간답니다. 꼭 저희에게 구매하지 않더라도, 시장가시기 전에 그 날 시세 보시면 좋을
             거에요. 간혹
           </span>
-          <div className="storeExtraInfo">
+          <div className="storeExtraInfo" tabIndex={0}>
             <span>후기 1,713</span>
             <span>단골 26,497</span>
             <span>수산물</span>
