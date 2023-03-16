08d7e6552a72d3ffca42b8a3847bf82e39e33756
Simyumi <gkghgkgh0@naver.com>
2023-03-16 16:58:47 +0900

    [코드수정] index.js파일 추가.

---
+++ src/pages/Mypage/index.js
@@ -0,0 +1,5 @@
+export * from "@/pages/Mypage/SidebarTop";
+export * from "@/pages/Mypage/MypageSection";
+export * from "@/pages/Mypage/SidebarBottom";
+export * from "@/pages/Mypage/SectionProfileInfo";
+export * from "@/pages/Mypage/MannerTemperature";
