0e39ebf06e6042396462ce8495207351558c29f5
rodwnl <88178123+rodwnl@users.noreply.github.com>
2023-03-16 22:17:14 +0900

    [기타] 스와이프 버그 확인

--- src/pages/Home/MainBanner.jsx
+++ src/pages/Home/MainBanner.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import styled from "styled-components";
 import {Swiper, SwiperSlide} from "swiper/react";
+import {Navigation, Pagination, Scrollbar, A11y, Autoplay} from "swiper";
 import {Link} from "react-router-dom";
 import BannerImg from "@/assets/banner01.svg";
 
 export function MainBanner() {
   return (
     <StyledBanner>
-      <Swiper>
+      <Swiper modules={[Autoplay]} autoplay={({speed: 500}, {disableOnInteraction: false})}>
         <SwiperSlide>
-          <Link to="/">
-            <img src={BannerImg} alt="동네가게 바로가기 배너" />
-          </Link>
+          <img src={BannerImg} alt="동네가게 바로가기 배너" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={BannerImg} alt="동네가게 바로가기 배너" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={BannerImg} alt="동네가게 바로가기 배너" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={BannerImg} alt="동네가게 바로가기 배너" />
         </SwiperSlide>
       </Swiper>
     </StyledBanner>
@@ -21,4 +29,22 @@ export function MainBanner() {
 const StyledBanner = styled.div`
   width: clamp(1040px, 100%, 1920px);
   margin: 80px auto;
+
+  & .swiper {
+    width: 100%;
+    height: 100%;
+  }
+
+  & .swiper-slide {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  & img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 `;
