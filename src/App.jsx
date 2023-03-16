f05f7e18a5a3b005d38e176ddff972eab48283bb
rodwnl <88178123+rodwnl@users.noreply.github.com>
2023-03-16 16:14:28 +0900

    [코드 수정] App.jsx 리팩토링

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-import {Home} from "./pages/Home/Home";
-
 function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
