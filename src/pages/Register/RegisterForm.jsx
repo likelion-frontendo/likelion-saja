4a0ce8f5526a2f164f996f8734b5528e7a98d437
rlawlsdn263 <rlawlsdn263@naver.com>
2023-03-17 17:44:11 +0900

    [코드 수정] RegisterForm attr, fieldset, legend 추가
    - action, method, legend 속성값 부여
    - fieldset, legend 태그 추가

--- src/pages/Register/RegisterForm.jsx
+++ src/pages/Register/RegisterForm.jsx
@@ -1,3 +1,8 @@
-export function RegisterForm({children, ...rest}) {
-  return <form {...rest}>{children}</form>;
+export function RegisterForm({action="/", method="post", legend, children, ...rest}) {
+  return <form action={action} method={method} {...rest}>
+    <fieldset>
+      <legend>{legend}</legend>
+    </fieldset>
+    {children}
+    </form>;
 }
