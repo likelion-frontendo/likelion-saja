11ed6133e33dc64ad017caed3bcfe104af65f632
rlawlsdn263 <rlawlsdn263@naver.com>
2023-03-18 14:34:27 +0900

    [스타일] Register 회원가입 페이지 컴포넌트 스타일 완성
    - Form, FormInput, Label, Button으로 상단 섹션 스타일을 완성하고 이용약관과 관련된 섹션은 별로도 만든 Terms 컴포넌트로 완성함
    - Header, Footer 공용 컴포넌트 추가
    - 아직 컴포넌트 분리가 완전하지 않아 리팩토링 작업이 필요함

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,21 +1,81 @@
-import { RegisterFormInput, RegisterForm, RegisterButton } from '@/pages/Register'
+import { RegisterFormInput, RegisterForm, RegisterButton, RegisterLabel, RegisterTerms } from '@/pages/Register'
+import { Header, Footer } from '@/components'
+import styled from 'styled-components'
 
 export function Register() {
   return(
-    <main className="register">
-      <h2>회원가입</h2>
-      <div>필수입력사항</div>
-      <RegisterForm legend="회원가입">
-        <RegisterFormInput label="이메일" name="email" type="email" placeholder="예시) frontendo@saja.com">
-          <RegisterButton>중복확인</RegisterButton>
-        </RegisterFormInput>
-        <RegisterFormInput label="비밀번호" name="password" type="password" placeholder="비밀번호를 입력해주세요" />
-        <RegisterFormInput label="비밀번호 확인" name="password" type="password" placeholder="비밀번호를 한번 더 입력해주세요" />
-        <RegisterFormInput label="이름" name="name" type="text" placeholder="이름을 입력해주세요" />
-        <RegisterFormInput label="휴대폰" name="mobile" type="text" placeholder="숫자만 입력해주세요">
-          <RegisterButton>인증번호</RegisterButton>
-        </RegisterFormInput>
-      </RegisterForm>
-    </main>
+    <>
+      <Header></Header>
+      <StyledMain className="register">
+        <h2>회원가입</h2>
+        <div className="description"><sup>*</sup>필수입력사항</div>
+        <RegisterForm legend="회원가입">
+          <RegisterFormInput className="formInput" label="이메일" name="email" type="email" placeholder="예시) frontendo@saja.com">
+            <RegisterButton>중복확인</RegisterButton>
+          </RegisterFormInput>
+          <RegisterFormInput label="비밀번호" name="password" type="password" placeholder="비밀번호를 입력해주세요" />
+          <RegisterFormInput label="비밀번호" name="password" type="password" placeholder="비밀번호를 입력해주세요" />
+          <RegisterFormInput label="이름" name="name" type="text" placeholder="이름을 입력해주세요" />
+          <RegisterFormInput label="휴대폰" name="mobile" type="text" placeholder="숫자만 입력해주세요">
+            <RegisterButton>인증번호 받기</RegisterButton>
+          </RegisterFormInput>
+          <RegisterFormInput label="생년월일" name="birthday" type="date"/>
+          <div className="addressWrapper">
+            <RegisterLabel>주소<sup>*</sup></RegisterLabel>
+            <RegisterButton style={{width: '333px', height: '44px'}}>주소 검색</RegisterButton>
+          </div>
+          <RegisterTerms></RegisterTerms>
+          <div className="registerButton" >
+            <RegisterButton style={{width: '340px', height: '54px', background: '#6C816D', color: 'white'}}>가입하기</RegisterButton>
+          </div>
+        </RegisterForm>
+      </StyledMain>
+      <Footer></Footer>
+    </>
   )
 }
+
+const StyledMain = styled.main `
+  width: 640px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  position: relative;
+
+  & h2 {
+    font-weight: 600;
+    font-size: 22px;
+    margin: 40px 0;
+  }
+
+  & .description {
+    text-align: right;
+    font-size: 12px;
+    font-weight: 600;
+    text-align: right;
+    width: 640px;
+    margin-bottom: 4px;
+    padding: 4px;
+  }
+
+  & .description > sup {
+    color: red;
+    position: relative;
+    top: -4px;
+    left: -4px;
+  }
+
+  .addressWrapper {
+    margin-bottom: 20px;
+  }
+
+  & .registerButton {
+    border-top: 1px solid black;
+    text-align: center;
+  }
+
+  & .registerButton > button {
+    margin: 40px 0;
+  }
+`
