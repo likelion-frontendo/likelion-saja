855d3ed1f3331d1036b1c762623b82cd3c8de04f
Sungchan CHo <kjyy07@naver.com>
2023-03-19 17:49:52 +0900

    [기타] img test 삭제

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import {Link} from "react-router-dom";
-import test from "@/assets/test.jpg";
 
 export function Product(props) {
   return (
