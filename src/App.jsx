f35a86f2f3dc6e2c60e77c5b280de46fde352d71
Sungchan CHo <kjyy07@naver.com>
2023-03-20 15:43:01 +0900

    [기타] 라우터 재설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { Routes, Route } from 'react-router-dom';
-import {Home, Login, Register} from '@/pages';
-import { Mypage } from './pages/Mypage/Mypage';
+import {Routes, Route} from "react-router-dom";
+import {Home, Login, Register} from "@/pages";
+import {Mypage} from "./pages/Mypage/Mypage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Register />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/mypage" element={<Mypage />} />
       </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
