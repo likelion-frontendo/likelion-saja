caafd3e735c1c8bd18ec6a21921fee03dbad244b
Sungchan CHo <kjyy07@naver.com>
2023-03-20 09:32:13 +0900

    [기타] import 변경

--- src/pages/Home/MainProduct.jsx
+++ src/pages/Home/MainProduct.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {Product, UseProductList} from "@/components";
+import {UseProductList} from "@/components";
 import {Link} from "react-router-dom";
 
 export function MainProduct() {
