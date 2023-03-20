567e292eb72f87917ac43b107366aab81757eb82
rodwnl <88178123+rodwnl@users.noreply.github.com>
2023-03-20 11:36:47 +0900

    [버그] Firebase productList에 location 추가 시 버그

--- src/components/Product/UseProductList.js
+++ src/components/Product/UseProductList.js
@@ -1,5 +1,5 @@
 import {app} from "@/firebase/app";
-import {getFirestore, collection, getDocs} from "firebase/firestore";
+import {getFirestore, collection, getDocs, where, query} from "firebase/firestore";
 import React, {useState, useEffect} from "react";
 import {Product} from "@/components";
 
@@ -13,9 +13,17 @@ export function UseProductList() {
     getDocs(productsRef)
       .then((querySnapshot) => {
         const productList = [];
-        querySnapshot.forEach((doc) => {
+        querySnapshot.forEach(async (doc) => {
           const product = {id: doc.id, ...doc.data()};
-          productList.push(product);
+          const userId = product.userId;
+
+          const userQuerySnapshot = await getDocs(query(collection(db, "Users"), where("id", "==", userId)));
+          userQuerySnapshot.forEach((userDoc) => {
+            const userData = userDoc.data();
+            const productWithUser = {...product, location: userData.location};
+            productList.push(productWithUser);
+          });
+          console.log(productList);
         });
         setProducts(productList);
       })
@@ -27,7 +35,7 @@ export function UseProductList() {
   return (
     <div className="productContainer">
       {products.map((product) => (
-        <Product key={product.id} imgUrl={product.imgUrl} title={product.title} price={product.price} location={"부산 북구 만덕제2동"} interest={product.interest} />
+        <Product key={product.id} imgUrl={product.imgUrl} title={product.title} price={product.price} location={product.location} interest={product.interest} />
       ))}
     </div>
   );
