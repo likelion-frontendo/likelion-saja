ecb6f910b4fea58879abcb729ebb837f4625f2f0
rodwnl <88178123+rodwnl@users.noreply.github.com>
2023-03-20 16:09:27 +0900

    [코드 수정] productList 값 불러오기 수정

--- src/components/Product/UseProductList.js
+++ src/components/Product/UseProductList.js
@@ -4,7 +4,25 @@ import React, {useState, useEffect} from "react";
 import {Product} from "@/components";
 
 export function UseProductList() {
+  const {isLoading, products} = useProducts();
+
+  if (isLoading) {
+    return <div role="alert">로딩 중...</div>;
+  }
+
+  return (
+    <div className="productContainer">
+      {products.map((product, index) => (
+        <Product key={index} imgUrl={product.imgUrl} title={product.title} price={product.price} location={product.location} interest={product.interest} />
+      ))}
+    </div>
+  );
+}
+
+function useProducts() {
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const db = getFirestore(app);
@@ -12,31 +30,45 @@ export function UseProductList() {
 
     getDocs(productsRef)
       .then((querySnapshot) => {
-        const productList = [];
-        querySnapshot.forEach(async (doc) => {
+        const queryPromises = [];
+        const products = [];
+
+        querySnapshot.forEach((doc) => {
           const product = {id: doc.id, ...doc.data()};
           const userId = product.userId;
+          products.push(product);
 
-          const userQuerySnapshot = await getDocs(query(collection(db, "Users"), where("id", "==", userId)));
-          userQuerySnapshot.forEach((userDoc) => {
-            const userData = userDoc.data();
-            const productWithUser = {...product, location: userData.location};
-            productList.push(productWithUser);
+          const queryPromise = new Promise(async (resolve, reject) => {
+            const usersSnapshot = await getDocs(query(collection(db, "Users"), where("id", "==", userId)));
+            resolve(usersSnapshot.docs);
           });
-          console.log(productList);
+
+          queryPromises.push(queryPromise);
         });
-        setProducts(productList);
+
+        Promise.all(queryPromises)
+          .then((userDocs) => {
+            const users = [];
+            userDocs.forEach(([doc]) => {
+              users.push({id: doc.id, ...doc.data()});
+            });
+            return users;
+          })
+          .then((users) => {
+            return users.map((user) => {
+              return {...products.find((product) => product.userId === user.id), location: user.location};
+            });
+          })
+          .then((usersWithProductInfo) => {
+            setProducts(usersWithProductInfo);
+            setIsLoading(false);
+          });
       })
       .catch((error) => {
         console.log("Error getting documents: ", error);
+        setError(error);
       });
   }, []);
 
-  return (
-    <div className="productContainer">
-      {products.map((product) => (
-        <Product key={product.id} imgUrl={product.imgUrl} title={product.title} price={product.price} location={product.location} interest={product.interest} />
-      ))}
-    </div>
-  );
+  return {isLoading, error, products};
 }
