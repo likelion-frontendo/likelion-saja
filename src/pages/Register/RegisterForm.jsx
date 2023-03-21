b19792b54014da58cb22dba062f60729be1768da
rlawlsdn263 <rlawlsdn263@naver.com>
2023-03-21 15:14:16 +0900

    [기능] 경고창 활성화 기능
    - 경고창으로 사용할 span에 .registerWarning과 .showWarning으로 스타일을 입혔음
    - 경고창을 조건에 따라 표시해주기 위해 불린형 상태변수인 visible을 만듦
    - 경고창의 텍스트를 조건에 따라 제시하기 위해 문자형 상태변수인 Warning을 만듦

--- src/pages/Register/RegisterForm.jsx
+++ src/pages/Register/RegisterForm.jsx
@@ -11,68 +11,101 @@ export function RegisterForm() {
   const [passwordConfirm, setPasswordConfirm] = useState('');
   const [name, setName] = useState('');
   const [mobile, setMobile] = useState('');
-  const [birthday, setBirthday] = useState('');
+  // const [birthday, setBirthday] = useState('');
+
+  const [emailVisible, setEmailVisible] = useState(false);
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [passwordConfirmVisible, setPasswordConfirmVisible] = useState(false);
+  const [mobileVisible, setMobileVisible] = useState(false);
+
+  const [emailWarning, setEmailWarning] = useState("");
+  const [passwordWarning, setPasswordWarning] = useState("");
+  const [passwordConfirmWarning, setPasswordConfirmWarning] = useState("");
+  const [mobileWarning, setMobileWarning] = useState("");
 
   function emailValidation(email){
     let emailRegex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+    let warningMessage = '';
+
     if(!emailRegex.test(email)) {
-      console.log('올바른 이메일 양식이 아닙니다');
+      warningMessage="올바른 이메일 형식이 아닙니다"
+      setEmailWarning(warningMessage);
+      setEmailVisible(true);
     } else {
-      console.log('이 이메일은 사용이 가능합니다');
+      setEmailVisible(false);
     };
   }
 
   function passwordValidation(password) {
+
     const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}$/;
+    let warningMessage = '';
+
     if (password.length < 8) {
-      console.log('비밀번호는 최소 8자리여야 합니다');
+      warningMessage = '비밀번호는 최소 8자리여야 합니다';
+      setPasswordVisible(true);
+      setPasswordWarning(warningMessage);
     } else if (password.length > 16) {
-      console.log('비밀번호는 최대 16자리여야 합니다');
+      warningMessage = '비밀번호는 최대 16자리여야 합니다';
+      setPasswordVisible(true);
+      setPasswordWarning(warningMessage);
     } else if (!passwordRegex.test(password)) {
-      console.log('비밀번호는 영어소문자, 영어대문자, 숫자, 특수기호가 들어가야 합니다');
+      warningMessage = '비밀번호는 영어소문자, 영어대문자, 숫자, 특수문자가 들어가야 합니다';
+      setPasswordVisible(true);
+      setPasswordWarning(warningMessage);
     } else {
-      console.log('이 비밀번호는 사용이 가능합니다');
+      warningMessage = '이 비밀번호는 사용이 가능합니다';
+      setPasswordVisible(false);
+      setPasswordWarning(warningMessage);
     }
+
+    // return warningMessage;
   }
 
   function passwordConfirmValidation(passwordConfirm) {
+
+    let warningMessage = '';
+
     if(passwordConfirm !== password) {
-      console.log("비밀번호가 다릅니다!")
-      console.log(password, passwordConfirm)
+      setPasswordConfirmVisible(true);
+      warningMessage = "비밀번호가 다릅니다!";
+      setPasswordConfirmWarning(warningMessage);
     } else {
-      console.log("비밀번호가 확인됐습니다!")
-      console.log(password, passwordConfirm)
+      setPasswordConfirmVisible(false);
     }
   }
 
   function mobileValidation(mobile) {
     const mobileRegex = /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/;
-    if(mobileRegex.test(mobile)) {
-      console.log('전화번호가 맞습니다.');
+    let warning = '';
+    
+    if(!mobileRegex.test(mobile)) {
+      warning = '올바른 휴대폰 형식이 아닙니다';
+      setMobileWarning(warning);
+      setMobileVisible(true);
+    } else {
+      setMobileVisible(false);
     }
   }
 
   function handleMobileValidation(e) {
     const mobile = e.target.value;
     const numberOnly = /^[0-9]*$/;
+    let warning = '';
 
     if(!numberOnly.test(mobile)) {
       e.target.value = mobile.slice(0, -1);
-      console.log('숫자를 입력해주세요');
+      warning = '숫자만 입력이 가능합니다';
+      setMobileWarning(warning);
+      setMobileVisible(true);
       return;
     }
 
-    /* if(mobile.length > 11) {
-      e.target.value = mobile.slice(0, -1);
-      console.log('11자리 이하의 전화번호를 입력해주세요');
-      return;
-    } */
-    
     setMobile(mobile);
     mobileValidation(mobile);
   }
 
-  function handleBirthdayYearValidataion(e) {
+  /* function handleBirthdayYearValidataion(e) {
     const year = e.target.value;
     const numberOnly = /^[0-9]*$/;
     if(!numberOnly.test(year)) {
@@ -80,12 +113,8 @@ export function RegisterForm() {
       return;
     }  
 
-    if(year.length < 4) {
-      console.log("생년월일을 정확하게 입력해주세요")
-    } else if(year < 1900) {
-      console.log('생년월일을 다시 확인해주세요');
-    } else if(year > 2100) {
-      console.log('생년월일을 다시 확인해주세요');
+    if(year < 1900 || year > 2100) {
+      console.log('연을 다시 확인해주세요');
     }
   }
 
@@ -97,28 +126,43 @@ export function RegisterForm() {
       return;
     } 
 
-    /* if(year.length < 2) {
-      console.log("태어난 년도 4자리를 정확하게 입력해주세요")
-    } else if(year < 1900) {
-      console.log('생년월일을 다시 확인해주세요');
-    } else if(year > 2100) {
-      console.log('생년월일을 다시 확인해주세요');
-    } */
+    if(month < 1 || month > 12) {
+      console.log('월을 다시 확인해주세요');
+    }
   }
+
+  function handleBirthdayDayValidataion(e) {
+    const day = e.target.value;
+    const numberOnly = /^[0-9]*$/;
+    if(!numberOnly.test(day)) {
+      e.target.value = day.slice(0, -1);
+      return;
+    } 
+
+    if(day < 1 || day > 31) {
+      console.log('월을 다시 확인해주세요');
+    }
+  } */
 
   return(
     <StyledSection className="registerTop">
       <Form className="registerForm" legend="회원가입">
         <RegisterFormInput label="이메일" name="email" type="email" placeholder="예시) frontendo@saja.com" onChange={(e) => {setEmail(e.target.value); emailValidation(email)}}>
           <Button className="registerButtonShort">중복확인</Button>
+          <span className={emailVisible === true ? "registerWarning showWarning" : "registerWarning"}>{emailWarning}</span>
         </RegisterFormInput>
-        <RegisterFormInput label="비밀번호" name="password" type="password" placeholder="비밀번호를 입력해주세요" onChange={(e) => {setPassword(e.target.value); passwordValidation(password);}} />
-        <RegisterFormInput label="비밀번호 확인" name="password" type="password" placeholder="비밀번호를 한번 더 입력해주세요" onChange={(e) => {setPasswordConfirm(e.target.value); passwordConfirmValidation(passwordConfirm);}}/>
+        <RegisterFormInput label="비밀번호" name="password" type="password" placeholder="비밀번호를 입력해주세요" onChange={(e)=>{setPassword(e.target.value); passwordValidation(password)}}>
+          <span className={passwordVisible === true ? "registerWarning showWarning" : "registerWarning"}>{passwordWarning}</span>
+        </RegisterFormInput>
+        <RegisterFormInput label="비밀번호 확인" name="password" type="password" placeholder="비밀번호를 한번 더 입력해주세요" onChange={(e) => {setPasswordConfirm(e.target.value); passwordConfirmValidation(passwordConfirm);}}>
+          <span className={passwordConfirmVisible === true ? "registerWarning showWarning" : "registerWarning"}>{passwordConfirmWarning}</span>
+        </RegisterFormInput>
         <RegisterFormInput label="이름" name="name" type="text" placeholder="이름을 입력해주세요" onChange={(e) => {setName(e.target.value);}} />
-        <RegisterFormInput label="휴대폰" name="mobile" type="text" placeholder="숫자만 입력해주세요" maxLength="11" onChange={handleMobileValidation /* {setMobile(e.target.value); mobileValidation(e.target.value)}*/}>
+        <RegisterFormInput label="휴대폰" name="mobile" type="text" placeholder="숫자만 입력해주세요" maxLength="11" onChange={handleMobileValidation}/*(e) => {setMobile(e.target.value); mobileValidation(e.target.value)}*/>
           <Button className="registerButtonShort">인증번호 받기</Button>
+          <span className={mobileVisible === true ? "registerWarning showWarning" : "registerWarning"}>{mobileWarning}</span>
         </RegisterFormInput>
-        <div style={{display: 'flex', alignItems: 'center',}}>
+        {/* <div style={{display: 'flex', alignItems: 'center',}}>
           <Heading3 className="registerHeading">
             <Label className="registerLabel">생년월일<sup>*</sup></Label>
           </Heading3>
@@ -127,15 +171,21 @@ export function RegisterForm() {
             <span>/</span>
             <input type="text" placeholder="MM" style={{width: '33%', height: "38px", textAlign: 'center', border: 'none'}} maxLength="2" onChange={handleBirthdayMonthValidataion}/>
             <span>/</span>
-            <input type="text" placeholder="DD" style={{width: '33%', height: "38px", textAlign: 'center', border: 'none'}} />
+            <input type="text" placeholder="DD" style={{width: '33%', height: "38px", textAlign: 'center', border: 'none'}} maxLength="2" />
           </div>
         </div>
-        <RegisterFormInput label="생년월일" name="year" type="text"/>
+        <RegisterFormInput label="생년월일" name="year" type="date"/> */}
         <div className="registerFormInput">
           <Heading3 className="registerHeading">
             <Label className="registerLabel">주소<sup>*</sup></Label>
           </Heading3>
           <Button className="registerButtonLong">주소 검색</Button>
+        </div>
+        <div className="registerFormInput">
+          <Heading3 className="registerHeading">
+            <Label className="registerLabel">프로필 사진<sup>*</sup></Label>
+          </Heading3>
+          <Button className="registerButtonLong">프로필 등록</Button>
         </div>
       </Form>
   </StyledSection>
@@ -147,9 +197,11 @@ const StyledSection = styled.section`
 
   & .registerFormInput {
     display: flex;
+    flex-wrap: wrap;
     align-items: center;
     margin: 20px 0;
     padding: 20px 0;
+    position: relative;
   }
 
   & .registerLabel {
@@ -193,5 +245,17 @@ const StyledSection = styled.section`
     background: #fff;
     border: 1px solid #6C816D;
     cursor: pointer;
+  }
+
+  & .registerWarning {
+    display: none;
+    position: absolute; 
+    left: 150px;
+    top: 75px;
+    color: red;
+  }
+
+  & .showWarning {
+    display: block;
   }
 `
---
+++ src/pages/Register/RegisterWarning.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export function RegisterWarning({children, ...rest}) {
+  return(
+    <Styledspan>{children}</Styledspan>
+  )
+}
+
+const Styledspan = styled.span`
+  
+`
