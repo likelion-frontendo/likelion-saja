96790c4813d9a37bdb446a4eb23d8081c26368a5
Sungchan CHo <kjyy07@naver.com>
2023-03-22 01:04:45 +0900

    [스타일] ProductDetail 폴더 및 파일 만들고 마크업하기

---
+++ src/pages/ProductDetail/ProductDetailBody.jsx
@@ -0,0 +1,126 @@
+import styled from "styled-components";
+import {Link} from "react-router-dom";
+import {UseProductList} from "@/components";
+
+export function ProductDetailBody(props) {
+  return (
+    <StyledProductDetail>
+      <section className="productImg">
+        <Link to="/">
+          <img className="mainImg" src={props.imgUrl} alt="상품 이미지"></img>
+        </Link>
+      </section>
+      <section className="userInfo"></section>
+      <hr />
+      <section className="productDescription">
+        <span className="title">{props.title} </span>
+        <span className="price">{props.price}벨 </span>
+        <span className="description">{props.description} </span>
+        <span className="interest">관심 {props.interest} </span>
+      </section>
+      <hr />
+      <section className="popularProduct">
+        <div className="textContainer">
+          <span>당근마켓 인기중고</span>
+          <Link to="/">더 구경하기</Link>
+        </div>
+        <UseProductList count={6} />
+      </section>
+    </StyledProductDetail>
+  );
+}
+
+const StyledProductDetail = styled.div`
+  & hr {
+    width: 678px;
+    border: 1px solid #eaebee;
+  }
+
+  & .productImg {
+    margin: 64px auto 0 auto;
+    width: 678px;
+    height: 564px;
+  }
+  & .productDescription {
+    width: 678px;
+    height: 160px;
+    margin: 36px auto;
+    line-height: 30px;
+  }
+
+  & .mainImg {
+    width: 678px;
+    height: 564px;
+    margin-bottom: 25px;
+    border-radius: 8px;
+  }
+
+  & .title {
+    font-style: normal;
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 36px;
+  }
+
+  & .price {
+    display: block;
+    font-style: normal;
+    color: #212529;
+    margin-top: 3px;
+    font-weight: 700;
+    font-size: 15px;
+    line-height: 60px;
+  }
+
+  & .description {
+    display: block;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 17px;
+    line-height: 27px;
+  }
+
+  & .interest {
+    display: block;
+    font-style: normal;
+    margin-top: 3px;
+    font-size: 12px;
+    line-height: 42px;
+    color: #868e96;
+  }
+  & .popularProduct {
+    width: 678px;
+    height: 830px;
+    margin: 0 auto;
+  }
+  & .textContainer {
+    display: flex;
+    width: 678px;
+    margin: 0 auto;
+    justify-content: space-between;
+  }
+
+  & .textContainer span,
+  a {
+    margin: 36px 0;
+  }
+
+  & .textContainer span {
+    font-style: normal;
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 26px;
+  }
+
+  & .textContainer a {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 15px;
+    line-height: 21px;
+    color: #ff8a3d;
+  }
+
+  & a:hover {
+    font-weight: 600;
+  }
+`;
