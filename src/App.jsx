d3112149ad86e112947a823b9d0cec40fd753237
Sungchan CHo <kjyy07@naver.com>
2023-03-21 17:25:29 +0900

    [기능] ProductDetail 페이지 라우터 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import {Routes, Route} from "react-router-dom";
-import {Home, Login, Register} from "@/pages";
+import {Home, Login, ProductDetail, Register} from "@/pages";
 import {Mypage} from "./pages/Mypage/Mypage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/mypage" element={<Mypage />} />
+        <Route path="/" element={<ProductDetail />} />
       </Routes>
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export * from "@/pages/Login/Login";
 export * from "@/pages/Home/Home";
 export * from "@/pages/Mypage/Mypage";
 export * from "@/pages/Register/Register";
+export * from "@/pages/ProductDetail/ProductDetail";
