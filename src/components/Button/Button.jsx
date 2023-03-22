4d0416adf47a1d4df760240a283629ffaa47287f
rlawlsdn263 <rlawlsdn263@naver.com>
2023-03-22 10:48:10 +0900

    [코드 수정] 버튼 컴포넌트 전역 스타일 제거
    - Register 컴포넌트의 스타일을 오염시켜 컴포넌트로 넣어둔 전역 스타일을 제거함

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,3 @@
-import styled from 'styled-components';
-
 export function Button({type="button", children, ...rest }) {
-  return <StyledButton type={type} {...rest}>{children}</StyledButton>;
+  return <button type={type} {...rest}>{children}</button>;
 }
-
-const StyledButton = styled.button`
-  color: #fff;
-  background-color: #a5b2a6;
-  &:hover {
-    background-color: #6c816d;
-  }
-`
