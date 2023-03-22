1ef0e79449330753caef76ec0fa9516fa9a3243c
kimmoonju-102 <kimmun3549@naver.com>
2023-03-22 13:33:53 +0900

    [기능] #60 로그인 기능구현
    - 로그인 실패 시 경고창을 띄워준다.

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,33 @@
 import styled from 'styled-components/macro';
-import { Button, LinkButton } from '@/pages/Login/index';
+import { Button, LinkButton, LoginModal } from '@/pages/Login/index';
 import { Form, Input, Label, Header, Footer, Heading2 } from '@/components/index';
+import { useState } from 'react';
+import {  signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/firebase/app";
 
 export function Login() {
+  const [loginEmail, setLoginEmail] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  
+  const login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+      console.log('로그인 성공!');
+      console.log(user);
+    } catch(error) {
+      setIsModalOpen(true);
+    }
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return(
     <>
     <Header></Header>
@@ -10,12 +35,17 @@ export function Login() {
       <Heading2 className="header">로그인</Heading2>
       <Form className="login" legend="로그인">
         <Label name="로그인"></Label>
-        <Input id="login" name="login" type="login" required placeholder="아이디를 입력해주세요."></Input>
+        <Input id="login" name="login" type="login" required placeholder="아이디를 입력해주세요." onChange={(e)=>{setLoginEmail(e.target.value)}}></Input>
         <Label name="비밀번호"></Label>
-        <Input id="password" name="password" type="password" required placeholder="비밀번호를 입력해주세요."></Input>
+        <Input id="password" name="password" type="password" required placeholder="비밀번호를 입력해주세요." onChange={(e)=>{setLoginPassword(e.target.value)}}></Input>
       </Form>
-      <Button type="button">로그인</Button>
+      <Button type="button" onClick={login}>로그인</Button>
       <LinkButton to="/register">회원가입</LinkButton>
+      {isModalOpen && (
+        <LoginModal isOpen={isModalOpen} onClose={closeModal}>
+          이메일,비밀번호를 확인해주세요.
+        </LoginModal>
+      )}
     </StyledMain>
     <Footer></Footer>
     </>
@@ -29,9 +59,9 @@ const StyledMain = styled.main `
   align-items: center;
   justify-content: center;
   
-  & .header {
-    padding: 40px;
-  }
+    & .header {
+      padding: 40px;
+    }
 
     & .login {
       padding-bottom: 29px;
@@ -39,26 +69,26 @@ const StyledMain = styled.main `
 
 
     & Label {
-    overflow: hidden;
-    position: absolute;
-    clip: rect(1px, 1px, 1px, 1px);
-    clip-path: circle(0);
-    width: 1px;
-    height: 1px;
-    margin: -1px;
-    border: 0;
-    padding: 0;
-    white-space: nowrap;
-  }
+      overflow: hidden;
+      position: absolute;
+      clip: rect(1px, 1px, 1px, 1px);
+      clip-path: circle(0);
+      width: 1px;
+      height: 1px;
+      margin: -1px;
+      border: 0;
+      padding: 0;
+      white-space: nowrap;
+    }
 
-  & Input {
-    display: flex;
-    width: 340px;
-    height: 50px;
-    font-size: 16px;
-    padding-left: 20px;
-    border: 1px solid #A6A6A6;
-    border-radius: 4px;
-    margin-bottom: 12px;
-  }  
+    & Input {
+      display: flex;
+      width: 340px;
+      height: 50px;
+      font-size: 16px;
+      padding-left: 20px;
+      border: 1px solid #A6A6A6;
+      border-radius: 4px;
+      margin-bottom: 12px;
+    }  
 `
