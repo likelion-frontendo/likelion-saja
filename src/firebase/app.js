fa6e7ee39d060a7ce4810684a1f8ee54604c2e91
rlawlsdn263 <rlawlsdn263@naver.com>
2023-03-22 16:50:38 +0900

    [기능] 파이어베이스 auth 인증 기능

--- src/firebase/app.js
+++ src/firebase/app.js
@@ -1,4 +1,5 @@
 import {initializeApp} from "firebase/app";
+import {getAuth} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCIkukWRJszdr7idpMABTC-lJKjZ2P1D9g",
@@ -10,3 +11,4 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
