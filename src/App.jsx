5818c8a76bce7031e5756dae5317b5f7376df8f6
Sungchan CHo <kjyy07@naver.com>
2023-03-22 09:58:28 +0900

    [병합] 상품상세페이지 병합

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/mypage" element={<Mypage />} />
-        <Route path="/" element={<ProductDetail />} />
+        <Route path="/productDetail" element={<ProductDetail />} />
         <Route path="/post" element={<Post />} />
       </Routes>
     </div>
