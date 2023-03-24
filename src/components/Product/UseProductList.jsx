a1b0bff7774978aa411d3f93b0402be516c4b660
Sungchan CHo <kjyy07@naver.com>
2023-03-24 16:13:18 +0900

    [기능] loading 창 만들기

--- src/components/Product/UseProductList.jsx
+++ src/components/Product/UseProductList.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {Product, useProducts} from "@/components";
+import raccoon from "@/assets/Logo/raccoon.gif";
+import styled from "styled-components";
 
 export function UseProductList(props) {
   const {isLoadingState, productsState} = useProducts();
+  const [showLoading, setShowLoading] = useState(true);
 
-  if (isLoadingState) {
-    return <div role="alert">로딩 중...</div>;
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoadingState || showLoading) {
+    return (
+      <StyledLoadingImgContainer role="alert">
+        <img src={raccoon} alt="로딩 중임..." />
+      </StyledLoadingImgContainer>
+    );
   }
 
   const slicedProducts = productsState.slice(0, props.count);
@@ -18,3 +32,13 @@ export function UseProductList(props) {
     </div>
   );
 }
+
+const StyledLoadingImgContainer = styled.div`
+  display: flex;
+  margin-top: 40px;
+  justify-content: center;
+
+  & img {
+    border-radius: 10%;
+  }
+`;
