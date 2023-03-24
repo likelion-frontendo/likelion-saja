5f01e08c929572b0cec0db196b124e0a46e74a99
Sungchan CHo <kjyy07@naver.com>
2023-03-24 14:27:03 +0900

    [스타일] 인기매물 페이지 마크업

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import {Routes, Route} from "react-router-dom";
-import {Home, Login, ProductDetail, Register, Mypage, Post} from "@/pages";
+import {Home, Login, ProductDetail, Register, Mypage, Post, PopularProduct} from "@/pages";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/mypage" element={<Mypage />} />
         <Route path="/productDetail/:id" element={<ProductDetail />} />
         <Route path="/post" element={<Post />} />
+        <Route path="/" element={<PopularProduct />} />
       </Routes>
     </div>
   );
---
+++ src/pages/PopularProduct/PopularProduct.jsx
@@ -0,0 +1,12 @@
+import {Header, Footer} from "@/components";
+import {PopularProductBody} from "./PopularProductBody";
+
+export function PopularProduct() {
+  return (
+    <div>
+      <Header />
+      <PopularProductBody />
+      <Footer />
+    </div>
+  );
+}
