8f538eab32c0027868bfd69706871a48311f1c17
rlawlsdn263 <rlawlsdn263@naver.com>
2023-03-26 22:01:55 +0900

    [버그] RequireAuth 오작동 버그
    - 로그인이 되어도 접근을 할 수 없는 버그가 발견됨

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import {Routes, Route} from "react-router-dom";
 import {Home, Login, ProductDetail, Register, Mypage, Post} from "@/pages";
-import { RequireAuth } from '@/components';
 
 function App() {
   return (
@@ -9,17 +8,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/mypage" element={
-          <RequireAuth>
-            <Mypage />
-          </RequireAuth>
-        } />
+        <Route path="/mypage" element={<Mypage />} />
         <Route path="/productDetail/:id" element={<ProductDetail />} />
-        <Route path="/post" element={
-          <RequireAuth>
-            <Post />
-          </RequireAuth>
-        } />
+        <Route path="/post" element={<Post />} />
       </Routes>
     </div>
   );
