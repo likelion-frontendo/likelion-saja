033e5f4ec149e18adababbc610790daf7249d077
rlawlsdn263 <rlawlsdn263@naver.com>
2023-03-28 17:39:14 +0900

    [기타] basename 설정

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
     <HelmetProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <RecoilRoot>
           <GlobalStyle />
           <App />
